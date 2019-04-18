cc22614b3fa7537d0a302543ff002e7c2761bba5
Eirin Sognnes <mev008@uib.no>
2019-04-18 10:44:25 +0200

    Create getInfo function with kommunenr argument

--- JS/JSONHandler.js
+++ JS/JSONHandler.js
@@ -1,43 +1,48 @@
-function Data(fileid){
-    this.fileid = fileid;
-    this.data = {};
-    this.onload = null;
+function Data(fileid) {
+  this.fileid = fileid;
+  this.data = {};
+  this.onload = null;
 
-    this.getNames = function(){
-      return Object.keys(this.data.elementer);
-    };
-    this.getIDs = function(){
-      var ret = [];
-      for(var i in this.data.elementer){
-        ret.push(this.data.elementer[i].kommunenummer);
+  this.getNames = function () {
+    return Object.keys(this.data.elementer);
+  };
+
+  this.getIDs = function () {
+    var ret = [];
+    for (var i in this.data.elementer) {
+      ret.push(this.data.elementer[i].kommunenummer);
+    }
+    return ret;
+  };
+
+  this.getInfo = function (knum) {
+    for (let kommunenavn in this.data.elementer) {
+      if(this.data.elementer[kommunenavn].kommunenummer == knum){
+        var kommune = {};
+        kommune[kommunenavn] = this.data.elementer[kommunenavn];
+        return kommune;
       }
-      return ret;
-    };
-    this.getInfo = function(){
-      for(var i in this.data.elementer){
-        if(this.data.elementer[i].kommunenummer === knum){
-          return this.data.elementer[i];
-        }
+    }
+  };
+
+  this.load = function (fileid, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://wildboy.uib.no/~tpe056/folk/" + fileid + ".json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        let response = JSON.parse(xhr.responseText);
+        callback(response);
+
       }
-      return null;
     };
-    this.load = function(fileid, callback){
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://wildboy.uib.no/~tpe056/folk/"+ fileid + ".json");
-            xhr.onreadystatechange = function(){
-            if(xhr.readyState == 4 && xhr.status == 200){
-                let response = JSON.parse(xhr.responseText);
-                callback(response);
-                // If user set onload, call it.
-                if(onload){
-                    onload();
-                }
-            }
-        };
-        xhr.send();
-    };
+    xhr.send();
+  };
 }
 
-let apple = new Data(104857);
-apple.onload = function(){console.log("HEYYYO");};
-apple.load(apple.fileid, function(input){apple.data = input;});
+let kommunedata = new Data(104857);
+kommunedata.load(kommunedata.fileid, function (input) {
+kommunedata.data = input;
+  if (kommunedata.onload) { kommunedata.onload() }
+});
+
+
