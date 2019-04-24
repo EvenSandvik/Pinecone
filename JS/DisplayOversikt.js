7afb11beb5725bc593656910d9eaef1da18487cf
Sondre Elde <Sondre.Elde@student.uib.no>
2019-04-24 09:53:20 +0200

    Begynt på ny fil displayOversikt

---
+++ JS/DisplayOversikt.js
@@ -0,0 +1,16 @@
+function overview(){
+  let kommuneBefolkning = befolkning.data.elementer;
+  let koms = befolkning.getNames();
+  koms.sort();
+  let alpha = ["A", "B", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "Ø", "Å"];
+
+  //Går gjennom alle bokstavane
+  for(let boks in alpha){
+
+
+  //Går gjennom alle kommunenavna
+  for(let navn in koms){
+
+  }
+}
+}
